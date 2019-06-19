b0479c78d1b338f82e9e0948e4061f9ab8619b5f
Allan Haldane <allan.haldane@gmail.com>
2019-06-19 17:53:01 -0400

    MAINT: Add some examples of MaskedArray subclasses

---
+++ subclass_examples/astrounit_masked.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+from MaskedArray import X, MaskedArray, MaskedScalar
+from astropy import units as u
+from astropy.units import m, km, s
+import numpy as np
+import duckprint
+
+class MaskedQ(MaskedArray):
+    known_types = MaskedArray.known_types + (u.quantity.Quantity,)
+
+    def __init__(self, data, *args, units=u.dimensionless_unscaled, **kwargs):
+        super().__init__(data, *args, **kwargs)
+        self._data = self._data*units
+
+    def __str__(self):
+        s = MaskedArray(np.array(self._data), self._mask)
+        return duckprint.duck_str(s) + " " + str(self.units)
+
+    def __repr__(self):
+        s = MaskedArray(np.array(self._data), self._mask)
+        return duckprint.duck_repr(s, name='MaskedQ', 
+                                   extra_args=['units={}'.format(self.units)])
+
+    @property
+    def units(self):
+        return self._data.unit
+
+    @units.setter
+    def units(self, val):
+        self._data.unit = val
+
+# TODO: scalars
+
+# TODO: allow binops directly with astropy.units.core.PrefixUnit
+
+if __name__ == '__main__':
+    uarr = MaskedQ([1, X, 1], units=km)
+    print(repr(uarr))
+    print(repr(uarr + (1*m)))
+    print(repr(uarr/(1*s)))
+    print(repr((uarr*(1*m))[1:]))
+    print(repr(np.add.outer(uarr, uarr)))
+    print(str(uarr))
---
+++ subclass_examples/unyt_masked.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+from MaskedArray import X, MaskedArray, MaskedScalar
+from unyt import m, km, s, unyt_array, unyt_quantity
+import numpy as np
+import duckprint
+
+class MaskedUnyt(MaskedArray):
+    known_types = MaskedArray.known_types + (unyt_array, unyt_quantity)
+
+    def __init__(self, data, *args, units=None, **kwargs):
+        super().__init__(data, *args, **kwargs)
+        self._data = unyt_array(self._data, units=units)
+
+    def __str__(self):
+        return duckprint.duck_str(self) + " " + str(self.units)
+
+    def __repr__(self):
+        return duckprint.duck_repr(self, extra_args=['units={}'.format(self.units)])
+
+    @property
+    def units(self):
+        return self._data.units
+
+    @units.setter
+    def units(self, val):
+        self._data.units = val
+
+# TODO: class MaskedQuantity(MaskedScalar):
+
+# TODO: allow binops with unyt.unit_object.Unit
+
+if __name__ == '__main__':
+    uarr = MaskedUnyt([1, X, 1], units=km)
+    print(repr(uarr))
+    print(repr(uarr + (1*m)))
+    print(repr(uarr/(1*s)))
+    print(repr((uarr*(1*m))[1:]))
+    print(repr(np.add.outer(uarr, uarr)))
+    print(str(uarr))
